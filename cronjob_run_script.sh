3b3bb44b2aad983d17acb9fe1ae9a568f5ae17eb
pkanpy <pkantlehner@gmail.com>
2024-10-30 03:30:01 -0400

    auto push via ev-charging webscraper

--- cronjob_run_script.sh
+++ cronjob_run_script.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-cd /root/my-documents/ev-charging-analysis
 git add .
 git commit -a -m "auto push from ev-charging webscraper"
 git push
-
-# 'cd ~ && cd /root/my-documents/ev-charging-analysis && git add . && git commit -a -m "auto push from ev-charging webscraper" && git push'
